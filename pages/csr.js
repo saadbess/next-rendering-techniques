d3db7424a0c40a6685a233036fd2f305b084c151
Ahmed elseadawi <Ahmed.elseadawi@seera.sa>
2022-03-15 12:15:24 +0400

    Update csr.js

--- pages/csr.js
+++ pages/csr.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Head from "next/head";
+import Nav from "../components/Nav";
+import Pokemon from "../components/Pokemon";
+import styles from "../styles/Ssg.module.css";
 
-export function CSR() {
-  return <h1>Hello CSR</h1>;
+export default function CSR() {
+  const [pokemon, setPokemon] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=50")
+      .then((res) => res.json())
+      .then((data) => {
+        setPokemon(data)
+        setLoading(false)
+      })
+  }, [])
+
+  if (isLoading) return <p>Loading...</p>
+  if (!pokemon) return <p>No Pokemon data</p>
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>CSR Pokemon</title>
+      </Head>
+      <Nav />
+      <div className={styles.pokemonList}>
+        {pokemon.results.map((monster, index) => (
+          <Pokemon key={index} pokemon={monster} index={index} />
+        ))}
+      </div>
+    </div>
+  )
 }
-
-export default CSR;
